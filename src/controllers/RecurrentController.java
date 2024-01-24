9cd37e65a2e4fd0ed0505e26f1f0a1e1a922ab29
poker <poker@DESKTOP-OO771KJ>
2024-01-24 12:24:37 +0100

    Commit de prueba

--- src/controllers/RecurrentController.java
+++ src/controllers/RecurrentController.java
@@ -1,4 +1,4 @@
-package controllers;
+  package controllers;
 
 import exceptions.CreateException;
 import exceptions.DeleteException;
--- src/controllers/SignInController.java
+++ src/controllers/SignInController.java
@@ -169,7 +169,7 @@ public class SignInController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/AccountView.fxml"));
             Parent root = loader.load();
             AccountController acc = loader.getController();
-            acc.setStage(thisStage);
+            acc.setStage(new Stage());
             acc.setUser(user);
             acc.initStage(root);
             thisStage.close();
@@ -183,6 +183,7 @@ public class SignInController {
         } catch (SelectException e) {
             this.showMessage(e.getMessage(), AlertType.ERROR);
         } catch (Exception e) {
+            e.printStackTrace();
             this.showMessage(e.getMessage(), AlertType.ERROR);
         }
     }
